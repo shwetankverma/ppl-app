3986b0a243b411b6823efb882be73b4dbff3fb9d
Shwetank Verma <shwetz888@gmail.com>
2026-06-13 12:17:21 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ppl-split-v6';
+const CACHE = 'ppl-split-v7';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
